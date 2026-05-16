2c593ba7b56efaa0947f99b9970490aea5e36ad5
sarahhalime <sarahhalime55@gmail.com>
2026-05-16 16:11:12 -0400

    completed up to task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,12 +8,12 @@ class FizzBuzz {
         // for (int i = 1; i < 100; i++) {
         int i = 1;
         while (i < 100) {
-            calcFizzBuzz(i);
+            doFizzBuzz(i);
             i++;
         }
     }
 
-    private static void calcFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -37,4 +37,5 @@ class FizzBuzz {
 
         }
     }
+
 }
